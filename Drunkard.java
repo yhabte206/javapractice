10ebf9f4e974a20df5f4b3f787953e36b7a819c5
yhabte206 <yhabte206@gmail.com>
2016-05-30 20:59:01 -0700

    first commit

---
+++ Drunkard.java
@@ -0,0 +1,46 @@
+// Name:
+// USC loginid:
+// CS 455 PA1
+// Spring 2016
+
+/**
+   Drunkard class
+       Represents a "drunkard" doing a random walk on a grid.
+*/
+public class Drunkard {
+
+    // add private instance variables here:
+
+
+
+
+    /**
+       Creates drunkard with given starting location and distance
+       to move in a single step.
+       @param startLoc starting location of drunkard
+       @param theStepSize size of one step in the random walk
+    */
+    public Drunkard(ImPoint startLoc, int theStepSize) {
+
+    }
+
+
+    /**
+       Takes a step of length step-size (see constructor) in one of
+       the four compass directions.  Changes the current location of the
+       drunkard.
+    */
+    public void takeStep() {
+
+    }
+
+
+    /**
+       gets the current location of the drunkard.
+       @return an ImPoint object representing drunkard's current location
+    */
+    public ImPoint getCurrentLoc() {
+	return null;  // REMOVE this line -- dummy code to get it to compile
+    }
+
+}
